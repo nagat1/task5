af834024dc0588bfbfd602071ba648bca39b784a
nagat1 <noga212008@outlook.com>
2025-04-05 21:42:52 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// var img=document.querySelector(".secondd");
+// window.addEventListener("load",function(){
+//     img.src=`imgs/673f572b42755f48fef10f07_Hero_1__1_.webp`
+//     img.style.width="100";
+//     img.style.width="70vh";
+// })
